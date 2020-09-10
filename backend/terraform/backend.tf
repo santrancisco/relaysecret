225b42950119114ecdad917d28c5915a980bc330
santrancisco <san.tran@ebfe.pw>
2020-09-10 18:41:14 +1000

    Fix backend.tf

--- backend/terraform/backend.tf
+++ backend/terraform/backend.tf
@@ -1,10 +1,5 @@
 
 terraform {
   required_version = "0.13.2"
-  backend "s3" {
-    region         = "ap-southeast-2"
-    key            = "relaysecret/terraform.tfstate"
-    bucket         = "san-acloud"
-  }
 }
 
