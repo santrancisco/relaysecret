07c085a95b32f59f330eccedf0ef5be2fd8c07b3
santrancisco <san.tran@ebfe.pw>
2020-09-11 10:12:01 +1000

    Added setting for www.relaysecret.com

--- assets/config.js
+++ assets/config.js
@@ -1,2 +1,2 @@
 // Change to your lambda endpoint here
-var lambdaurl = "https://{id}.execute-api.us-east-1.amazonaws.com/alpha/";
+var lambdaurl = "https://hv21tcnxw9.execute-api.us-east-1.amazonaws.com/alpha/";
